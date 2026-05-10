a8190b2e46e0311844128063713ddf3d8d2e7a61
RoAAlaniz <rocioabril.alaniz@estudiantes.unahur.edu.ar>
2026-05-10 13:57:26 -0300

    Parte materias.

--- controllers/materias.controller.js
+++ controllers/materias.controller.js
@@ -4,13 +4,29 @@ const getMaterias = async (req, res)=>{ //Es async porque va a trabajar con oper
   const materias = await Materia.findAll({}) //await espera a que termine la consulta antes de continuar.
   console.log(materias) //Muestra las materias en la consola del servidor.
     res.status(200).json(materias) //Envía la respuesta al cliente. 200 significa “OK”.
-}
+  }
+
 const getMateriaById = (req, res)=>{ //Función para obtener una materia por ID.
-    const id = req.params.id //Obtiene el parámetro id de la URL.
-    res.status(200).json({id}) //Envía la respuesta al cliente. 200 significa “OK”.
+  try{
+    const id = req.params.id
+    const materias = await Materia.findByPk(id) //await espera a que termine la consulta antes de continuar.
+    res.status(200).json(materias)
+  } catch(err){
+    res.status(500).json({message: err.message})
+  }
 }
 
-module.exports = {getMaterias, getMateriaById} //Exporta las funciones para poder usarlas en otro archivo.
+const createMateria = (req, res)=>{
+  try{
+    const nuevaMateria = req.body
+    const materias = await Materia.create(nuevaMateria) 
+    res.status(200).json(materias)
+  }catch(err){
+    res.status(500).json({message: "Error al crear materia"})
+  }
+}
+
+module.exports = {getMaterias, getMateriaById, createMateria}
 
 //req = request (lo que llega del cliente).
 //res = response (lo que se devuelve al cliente).
--- routes/materias.route.js
+++ routes/materias.route.js
@@ -1,10 +1,12 @@
 const {Router} = require('express') //Importa Router desde Express
-const {getMaterias, getMateriaById} = require('../controllers/materias.controller') //Importa funciones del controlador
+const {getMaterias, getMateriaById, createMateria} = require('../controllers/materias.controller') //Importa funciones del controlador
 
 const route = Router() //Crea un objeto router, aca se guardan las rutas
 
 route.get('/materias', getMaterias)
 route.get('/materias/:id', getMateriaById)
+route.post('/materias', createMateria)
+
 
 module.exports = route //Exporta las rutas.
 
